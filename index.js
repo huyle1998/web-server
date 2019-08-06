1e056bb8bd2d708682c1035de4cbbc5f8cbb7e1d
HNB <hnbandomachi@gmail.com>
2019-08-06 22:34:07 +0700

    Studying...4

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var port = 3000;
 
 var users = [
     { id: 1, name: 'Huy' },
-    { id: 2, name: 'Diem' }
+    { id: 2, name: 'Diem' },
+    { id: 3, name: 'Man' },
+    { id: 4, name: 'Ngan' },
+    { id: 5, name: 'Hai' },
+    { id: 6, name: 'Thanh'}
 ]
 
 app.set('view engine', 'pug')
-app.set('views', './views')
 
 app.get('/', function (req, res) {
     res.render('indexExpress');
@@ -22,12 +25,20 @@ app.get('/users', function (req, res) {
 
 app.get('/users/search', function (req, res) {
     var q = req.query.q;
+    console.log(q)
+    // document.getElementById("search").innerHTML = q;
     var matcheUsers = users.filter(function(users) {
         return users.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
     res.render('users/index', {
-        users: matcheUsers
+        users: matcheUsers,
+        search: q
     });
+    
+});
+
+app.get('/users/create', function(req, res) {
+    res.render('users/create.pug');
 });
 
 app.listen(port, function () {
